d549afdb763ec14c542f91b0241bd83e4a520f07
Mike Dahle <gotmugen@gmail.com>
2021-10-18 08:32:10 -0600

    added code journal, new index and style for SW

---
+++ codejournal.js
@@ -0,0 +1,32 @@
+/* this a a multi line comment*/
+
+//single line comment
+
+const ANSWER = 42;  // constant
+
+let string1 = "Hello World" //preferred way to declare string
+
+let string2 = new String("Hello World") //string using constructor
+
+//you dont have to distinguish between int and float
+
+"1" == 1 //this will show true, because its loosely checking
+
+"2" === 2 //this is stricly checking data type equality
+
+let myBool = false //boolean
+
+let exampleArray = [1, 'hello', 23.9 ] //array
+
+//arrays work similarly to python
+
+//objects
+
+let myCar = {
+    make: 'mazda',
+    year: '2007',
+    color: 'black',
+    doors: 4 
+} // like a dictionary in python | can define it with const or let
+
+myCar.doors = '4';
