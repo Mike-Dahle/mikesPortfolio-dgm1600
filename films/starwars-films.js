c07a981baec32be2765a547ee60623f36bc9b9a7
Mike Dahle <gotmugen@gmail.com>
2021-10-19 08:58:00 -0600

    Code is now current. added films.js and s-f.js

---
+++ films/starwars-films.js
@@ -0,0 +1,19 @@
+import { films } from '../data/films.js'
+
+let filmList = document.querySelector('#filmList')
+
+let titleList = document.createElement('ol')
+
+filmList.appendChild(titleList)
+
+let poster = document.createElement('img')
+
+poster.src = 'https://starwars-visualguide.com/assets/img/films/1.jpg'
+
+filmList.appendChild(poster)
+
+for (let i = 0; i < films.length; i++) {
+    let titleItem = document.createElement('li')
+    titleItem.textContent = films[i].title
+    titleList.appendChild(titleItem)
+}
