c06a1ce9291e81872a6b60bd58aa81fd91792331
Mike Dahle <gotmugen@gmail.com>
2021-10-26 09:37:10 -0600

    +some char css, and add new func to get lastnum

--- characters/sw-characters.js
+++ characters/sw-characters.js
@@ -5,14 +5,20 @@ const main = document.querySelector("main");
 people.forEach((element, index) => {
   const personFig = document.createElement("figure");
   const personImg = document.createElement("img");
-  personImg.src = `https://starwars-visualguide.com/assets/img/characters/${
-    index + 1
-  }.jpg`;
+  let lastNum = charNum(element.url);
+  personImg.src = `https://starwars-visualguide.com/assets/img/characters/${lastNum}.jpg`;
   const personCaption = document.createElement("figcaption");
-  personCaption.textContent = people[index].name;
+  personCaption.textContent = element.name;
+  console.log(personImg)
 
   personFig.appendChild(personImg);
   personFig.appendChild(personCaption);
 
   main.appendChild(personFig);
 });
+
+function charNum(url) {
+  let urlArray = url.split('/');
+  let last = urlArray[urlArray.length - 2]
+  return last
+}
