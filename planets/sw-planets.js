a7c574aa91e7bb815c206b33393984d38488e561
Mike Dahle <gotmugen@gmail.com>
2021-10-27 08:32:30 -0600

    added more js data files and made a planet page

---
+++ planets/sw-planets.js
@@ -0,0 +1,20 @@
+import { planets } from "../data/planets.js";
+import { getNum } from "../utils/index.js"
+
+let planetsList = document.querySelector("#planetsList");
+
+for (let i = 0; i < planets.length; i++) {
+  let figure = document.createElement("figure");
+  let figImg = document.createElement("img");
+  figImg.src = `https://starwars-visualguide.com/assets/img/planets/${i + 1}.jpg`;
+  let figCaption = document.createElement("figcaption");
+
+  const foundPlanet = planets.find(planet => getNum(planet.url) === (i + 1).toString())
+
+  figCaption.textContent = foundPlanet.name;
+
+  figure.appendChild(figImg);
+  figure.appendChild(figCaption);
+
+  planetsList.appendChild(figure);
+}
