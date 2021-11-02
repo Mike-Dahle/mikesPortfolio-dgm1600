5ec257e9a8407e8b350089971903e6122f6ab184
Mike Dahle <gotmugen@gmail.com>
2021-11-02 15:56:17 -0600

    Changed som imgs and worked on starships

--- starships/sw-starships.js
+++ starships/sw-starships.js
@@ -1,32 +1,48 @@
 import { starships } from "../data/starships.js";
-import { getNum } from "../utils/index.js";
-import { removeChildren } from "../utils/index.js";
+import { getNum, removeChildren } from "../utils/index.js";
 
-const nav = document.querySelector('.nav')
-const navList = document.querySelector('.navList')
-const shipView = document.querySelector('.shipViewer')
+const nav = document.querySelector(".nav");
+const navList = document.querySelector(".navList");
+const shipView = document.querySelector(".shipViewer");
+
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".modal-close");
+const modalBackground = document.querySelector(".modal-background");
+
+closeButton.addEventListener("click", () =>
+  modal.classList.toggle("is-active")
+);
+modalBackground.addEventListener("click", () =>
+  modal.classList.toggle("is-active")
+);
 
 function populateNav() {
-  starships.forEach(starship => {
-    let anchorWrap = document.createElement('a')
-    anchorWrap.href = '#'
-    let listItem = document.createElement('li')
-    listItem.textContent = starship.name
+  starships.forEach((starship) => {
+    let anchorWrap = document.createElement("a");
+    anchorWrap.href = "#";
+    let listItem = document.createElement("li");
+    listItem.textContent = starship.name;
 
-    anchorWrap.addEventListener('click', () => populateShipView(starship))
+    anchorWrap.addEventListener("click", () => populateShipView(starship));
 
-    anchorWrap.appendChild(listItem)
-    navList.appendChild(anchorWrap)
-  })
+    anchorWrap.appendChild(listItem);
+    navList.appendChild(anchorWrap);
+  });
 }
 
-populateNav()
+populateNav();
 
 function populateShipView(shipData) {
-  removeChildren(shipView)
-  console.log(`You clicked on ${shipData.name}`)
-  let shipImage = document.createElement('img')
-  let shipNum = getNum(shipData.url)
-  shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
-  shipView.appendChild(shipImage)
+  removeChildren(shipView);
+  let shipImage = document.createElement("img");
+  let shipName = document.querySelector('div')
+  //shipName.textContent = shipData.name
+  let shipNum = getNum(shipData.url);
+  shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`;
+  shipImage.addEventListener("error", () => {
+    shipImage.hidden = true;
+    modal.classList.toggle("is-active");
+  });
+  shipView.appendChild(shipImage);
+  //shipView.appendChild(shipName)
 }
