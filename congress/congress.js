adb2aedbd0ab705e6c4715a90fb7d3a1d648b337
Mike Dahle <gotmugen@gmail.com>
2021-11-04 15:28:34 -0600

    started a congress folder and adde rep/sen data

---
+++ congress/congress.js
@@ -0,0 +1,9 @@
+import { senators } from '../data/senators.js'
+
+const senatorsDiv = document.querySelector('.senators')
+
+function populateSenatorDiv() {
+    senators.forEach(senators => {
+        let senFigure = document.createElement('figure')
+    })
+}
