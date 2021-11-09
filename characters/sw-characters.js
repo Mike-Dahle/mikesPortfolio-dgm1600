4ce7b68c4afb070fa9ed24403dbeef95db222f30
Mike Dahle <gotmugen@gmail.com>
2021-11-08 18:01:35 -0700

    added a back button to chars and worked style

--- characters/sw-characters.js
+++ characters/sw-characters.js
@@ -1,10 +1,17 @@
 import { people } from "../data/people.js";
 import { getNum } from "../utils/index.js";
 
-const main = document.querySelector("main");
+const main = document.querySelector("#charView");
 
-const mainHeader = document.createElement('header')
+const mainHeader = document.createElement('nav')
+
 document.body.insertBefore(mainHeader, main)
+/*
+const homeButton = document.createElement('button')
+homeButton.textContent = 'Home'
+mainHeader.appendChild(homeButton)
+homeButton.addEventListener('click', () => '../index.html')
+*/
 
 const allButton = document.createElement('button')
 allButton.textContent = 'All Characters'
