54d53d74cae094ecf648990785d7df5e56b42468
Mike Dahle <gotmugen@gmail.com>
2021-12-01 13:44:37 -0700

    added unique backgrounds to cards + style

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -6,10 +6,11 @@ function getAPIData(url) {
   }
 }
 
-getAPIData("https://pokeapi.co/api/v2/pokemon?limit=151")
-.then( async (data) => {
+getAPIData("https://pokeapi.co/api/v2/pokemon?limit=151").then(async (data) => {
   for (const pokemon of data.results) {
-    await getAPIData(pokemon.url).then((pokeData) => populatePokeCards(pokeData));
+    await getAPIData(pokemon.url).then((pokeData) =>
+      populatePokeCards(pokeData)
+    );
   }
 });
 
@@ -35,12 +36,60 @@ function populatePokeCards(singlePokemon) {
 
 function populateCardFront(pokemon) {
   const pokeFront = document.createElement("figure");
+  const cardFront = document.querySelector(".front");
   pokeFront.className = "cardFace front";
+
+  //console.log(pokemon.types[0].type.name);
+
+  if (pokemon.types[0].type.name === "grass") {
+    pokeFront.style.backgroundImage =
+      "url(../images/cards/grass-card.png), url(../images/forest-bg.jpeg)";
+  } else if (pokemon.types[0].type.name === "fire") {
+    pokeFront.style.backgroundImage =
+      "url(../images/cards/fire-card.png), url(../images/forest-bg.jpeg)";
+  } else if (pokemon.types[0].type.name === "water") {
+    pokeFront.style.backgroundImage =
+      "url(../images/cards/water-card.png), url(../images/forest-bg.jpeg)";
+  } else if (pokemon.types[0].type.name === "bug") {
+    pokeFront.style.backgroundImage =
+      "url(../images/cards/grass-card.png), url(../images/forest-bg.jpeg)";
+  } else if (pokemon.types[0].type.name === "poison") {
+    pokeFront.style.backgroundImage =
+      "url(../images/cards/grass-card.png), url(../images/forest-bg.jpeg)";
+  } else if (pokemon.types[0].type.name === "electric") {
+    pokeFront.style.backgroundImage =
+      "url(../images/cards/electric-card.png), url(../images/forest-bg.jpeg)";
+  } else if (pokemon.types[0].type.name === "ground") {
+    pokeFront.style.backgroundImage =
+      "url(../images/cards/fighting-card.png), url(../images/forest-bg.jpeg)";
+  } else if (pokemon.types[0].type.name === "fighting") {
+    pokeFront.style.backgroundImage =
+      "url(../images/cards/fighting-card.png), url(../images/forest-bg.jpeg)";
+  } else if (pokemon.types[0].type.name === "psychic") {
+    pokeFront.style.backgroundImage =
+      "url(../images/cards/psychic-card.png), url(../images/forest-bg.jpeg)";
+  } else if (pokemon.types[0].type.name === "rock") {
+    pokeFront.style.backgroundImage =
+      "url(../images/cards/fighting-card.png), url(../images/forest-bg.jpeg)";
+  } else if (pokemon.types[0].type.name === "ghost") {
+    pokeFront.style.backgroundImage =
+      "url(../images/cards/psychic-card.png), url(../images/forest-bg.jpeg)";
+  } else if (pokemon.types[0].type.name === "dragon") {
+    pokeFront.style.backgroundImage =
+      "url(../images/cards/dragon-card.png), url(../images/forest-bg.jpeg)";
+  } else if (pokemon.types[0].type.name === "ice") {
+    pokeFront.style.backgroundImage =
+      "url(../images/cards/water-card.png), url(../images/forest-bg.jpeg)";
+  }  else {
+    pokeFront.style.backgroundImage =
+      "url(../images/cards/poke-card.png), url(../images/forest-bg.jpeg)";
+  }
+
   const pokeImg = document.createElement("img");
-  const pokeName = document.createElement('h2')
-  pokeName.textContent = pokemon.name
+  const pokeName = document.createElement("h2");
+  pokeName.textContent = pokemon.name;
   pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
-  pokeFront.appendChild(pokeName)
+  pokeFront.appendChild(pokeName);
   pokeFront.appendChild(pokeImg);
 
   return pokeFront;
@@ -49,15 +98,15 @@ function populateCardFront(pokemon) {
 function populateCardBack(pokemon) {
   const pokeBack = document.createElement("div");
   pokeBack.className = "cardFace back";
-  const label = document.createElement('h4')
-  label.textContent = 'Abilities'
-  pokeBack.appendChild(label)
-  const abilityList = document.createElement('ul')
+  const label = document.createElement("h4");
+  label.textContent = "Abilities";
+  pokeBack.appendChild(label);
+  const abilityList = document.createElement("ul");
   pokemon.abilities.forEach((abilityItem) => {
-      let listItem = document.createElement('li')
-      listItem.textContent = abilityItem.ability.name
-      abilityList.appendChild(listItem)
-  })
-  pokeBack.appendChild(abilityList)
+    let listItem = document.createElement("li");
+    listItem.textContent = abilityItem.ability.name;
+    abilityList.appendChild(listItem);
+  });
+  pokeBack.appendChild(abilityList);
   return pokeBack;
 }
