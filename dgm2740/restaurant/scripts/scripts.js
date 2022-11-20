2878bd6d5fd4fb1ff2920e95e2186089d6022d9a
Mike Dahle <gotmugen@gmail.com>
2022-11-20 01:11:29 -0700

    Restaurant site finished

---
+++ dgm2740/restaurant/scripts/scripts.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open")
+    document.getElementById("hamburgerBtn").classList.toggle("open")
+}
+
+const x = document.getElementById("hamburgerBtn")
+
+x.onclick = toggleMenu
