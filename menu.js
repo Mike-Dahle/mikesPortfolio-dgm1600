c4afa94636b555d08a4f18c226fde9dc5b9718ae
Mike Dahle <m_shizzle@live.com>
2023-03-15 15:43:27 -0600

    removed mobile menu

--- menu.js
+++
@@ -1,5 +0,0 @@
-const hb = document.querySelector('#hamburgerBtn');
-const pw = document.querySelector('#pageWrapper');
-hb.addEventListener('click', () => {
-    pw.classList.toggle('moveOver');
-});
