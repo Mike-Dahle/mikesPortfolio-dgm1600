1d512e676b452ec29ef7576a10f29eaa554d5c9e
Michael Dahle <91270944+Mike-Dahle@users.noreply.github.com>
2023-09-20 10:58:48 -0600

    added more functionality to the to-do list

--- data/todolist.js
+++ data/todolist.js
@@ -4,22 +4,36 @@ const todoList = document.querySelector('.todoList')
 const pendingText = document.querySelector('.pending')
 const deleteBtn = document.querySelector('.delete')
 
-const tasks = [
-    
-    ]
+// Need Add, Edit, Delete, and Clear Done to work.
 
-populatetodoList()
+const tasks = []
 
 pendingText.textContent = `You have ${pendingTasks(tasks)} remaining task(s)`
 
-deleteBtn.addEventListener('click', () => deleteTasks)
+deleteBtn.addEventListener('click', () => deleteTasks(tasks, todoList))
 
-function populatetodoList() {
+
+// Populate the todo list
+function populatetodoList(tasks) {
     tasks.forEach((task) => {
         const li = document.createElement('li')
+        const spanOne = document.createElement('span');
+        const spanTwo = document.createElement('span');
+        const trashIcon = document.createElement('i');
+        const editIcon = document.createElement('i');
+        li.addEventListener('click', () => {li.classList.toggle('done')})
+
+        trashIcon.classList.add('fa', 'fa-trash');
+        editIcon.classList.add('fa', 'fa-edit');
+        spanOne.classList.add('editBtn');
 
         li.textContent = task.todo
+        spanOne.appendChild(editIcon);
+        spanTwo.appendChild(trashIcon);
+        li.appendChild(spanOne);
+        li.appendChild(spanTwo);
         todoList.appendChild(li)
+        console.log(task)
     })
 }
 
@@ -35,13 +49,13 @@ addTask.addEventListener('click', () => {
     createTask(input.value)
     input.value = ''
     todoList.innerHTML = ''
-    populatetodoList()
+    populatetodoList(tasks)
     pendingText.textContent = `You have ${pendingTasks(tasks)} remaining task(s)`
 })
 
-function deleteTasks(arr) {
-    arr = []
-    populatetodoList()
+function deleteTasks(todoList) {
+});
+    
 }
 
 
