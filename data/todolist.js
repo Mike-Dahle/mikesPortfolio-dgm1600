e0bceb6570b5a1585bd750552bee5a66df23ce9c
Michael Dahle <91270944+Mike-Dahle@users.noreply.github.com>
2023-10-19 16:50:50 -0600

    creating modal for edit

--- data/todolist.js
+++ data/todolist.js
@@ -9,12 +9,13 @@ const clearBtn = document.querySelector('.clear')
 
 const tasks = []
 
+
 // Create a new task
 function createTask(task) {
     tasks.push({
       todo: task,
       done: false,
-      Id: tasks.length,
+      Id: Date.now(),
       editing: false
     },)
   }
@@ -34,26 +35,24 @@ function populatetodoList(tasks) {
     // Create list elements for each todo
     tasks.forEach((task) => {
         const li = document.createElement('li')
-        li.id = task.Id
+        li.setAttribute('id', task.Id)
         const spanOne = document.createElement('span');
         const spanTwo = document.createElement('span');
         const trashIcon = document.createElement('i');
         const editIcon = document.createElement('i');
-        const completedIcon = document.createElement('i');
+        const taskText = document.createElement('p');
+        const modal = document.getElementById("myModal");
+        const closeModal = document.getElementsByClassName("close")[0];
+        taskText.textContent = task.todo;
 
         trashIcon.classList.add('fa', 'fa-trash');
         editIcon.classList.add('fa', 'fa-edit');
         spanOne.classList.add('editBtn');
 
-        li.textContent = task.todo;
+
+        li.appendChild(taskText);
         li.appendChild(spanOne);
         li.appendChild(spanTwo);
-        li.appendChild(completedIcon);
-        li.addEventListener('click', (event) => {
-            if (event.target === li) {
-                markDone(li.textContent);
-            }
-        });
 
         // check if task is done, if so add the done class
         if (task.done === true) {
@@ -62,7 +61,7 @@ function populatetodoList(tasks) {
 
         spanOne.appendChild(editIcon);
         spanTwo.appendChild(trashIcon);
-        spanOne.addEventListener('click', () => editTask(li.textContent))
+        spanOne.addEventListener('click', () => {modal.style.display = "block";})
         spanTwo.addEventListener('click', (event) => {
             const li = event.target.closest('li'); // Find the closest parent 'li' element
             if (li) {
@@ -80,18 +79,15 @@ function populatetodoList(tasks) {
 }
 
 // Mark a task as done
-function markDone(taskName) {
-    const taskToUpdate = tasks.find((task) => task.todo === taskName);
+function markDone(finishedID) {
+
+    const taskToUpdate = tasks.find((task) => task.id === finishedID);
 
     if (taskToUpdate) {
         taskToUpdate.done = !taskToUpdate.done; // Toggle the 'done' property
         populatetodoList(tasks);
     }
 }
-
-
-
-
 
 // Add a new task to the list
 addTask.addEventListener('click', () => {
@@ -114,11 +110,9 @@ function deleteTask(text) {
     pendingText.textContent = `You have ${pendingTasks(tasks)} remaining task(s)`
 }
       
-function editTask() {
-    const editBtn = document.querySelector('.editBtn')
-    editBtn.addEventListener('click', () => {
-        console.log('edit')
-    })
+function editTask(id) {
+    editingItem = id;
+    editingItem.contentEditable = true; 
 }
 
 function clearDone(tasks) {
@@ -136,3 +130,11 @@ function pendingTasks(arr) {
     return arr.filter((arr) => arr.done === false).length
 }
 
+function markDone(finishedID) {
+    const taskToUpdate = tasks.find((task) => task.Id == finishedID);
+
+    if (taskToUpdate) {
+        taskToUpdate.done = !taskToUpdate.done; // Toggle the 'done' property
+        populatetodoList(tasks);
+    }
+}
